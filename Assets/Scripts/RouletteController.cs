dcca00bb97641850956f1b2dd1fe777eb2514e27
sarukiti_ch <sarukiti1891@gmail.com>
2020-07-16 22:07:07 +0900

    UnityプロジェクトごとGithubにうｐ GitLFSに対応

---
+++ Assets/Scripts/RouletteController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RouletteController : MonoBehaviour
+{
+    float speed = 0;
+    float r;
+
+    void Start(){
+        Random.InitState(System.DateTime.Now.Second);
+        r = Random.Range (30.0f, 1000.00f);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)){
+            this.speed = -r;
+        }
+            transform.Rotate(0, 0, this.speed);
+            this.speed *= 0.98f;
+    }
+}
---
+++ Assets/Scripts/RouletteGenerator.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RouletteGenerator : MonoBehaviour
+{
+        [SerializeField]
+        GameObject canvas;
+
+        [SerializeField]
+        GameObject hogehoge;
+
+        Image image;
+
+        [SerializeField]
+        int stageCount;
+
+        float amountSize;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        amountSize = 1.0f / stageCount;
+        int i = 1;
+        float c = 0f;
+        float rotateCount = 360.0f/stageCount;
+        while (i<=stageCount)
+        {
+            GameObject prefab = (GameObject)Instantiate(hogehoge);
+            prefab.transform.SetParent(canvas.transform, false);
+            prefab.name = $"image{i}";
+            image = GameObject.Find ($"image{i}").GetComponent<Image>();
+            image.fillAmount = amountSize;
+            image.transform.Rotate(0.0f, 0.0f, c);
+            float R = Random.Range (0.0f, 255.0f);
+            float B = Random.Range (0.0f, 255.0f);
+            image.color = new Color(R/255.0f, 160.0f/255.0f, B/255.0f, 255.0f/255.0f);
+            Transform tText = prefab.transform.Find("Text");
+            Text text = tText.GetComponent<Text>();
+            text.text = ($"{i}");
+            c -= rotateCount;
+            i++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
